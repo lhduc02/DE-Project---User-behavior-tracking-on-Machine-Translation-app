6ef97283dbc7901a40b3d9bed66ade40b4de7e7d
lehuynhduc <lhduc02@gmail.com>
2025-03-19 11:07:29 +0700

    update topic_3

--- code/kafka_producer.py
+++ code/kafka_producer.py
@@ -7,21 +7,18 @@ import time
 KAFKA_BROKER = "localhost:9092"
 TOPIC_1 = "topic_1"
 TOPIC_2 = "topic_2"
+TOPIC_3 = "topic_3"
 
 # Khởi tạo Kafka Producer
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BROKER,
-    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
-print(f"Kafka Producer started, sending 10 messages to {TOPIC_1} or {TOPIC_2} based on event type...")
+print(f"Kafka Producer started, sending messages to topic 1, topic 2 or topic 3 based on event type...")
 
 # Gửi dữ liệu 10 lần
-<<<<<<< HEAD
-for _ in range(100):
-=======
-for _ in range(1000):
->>>>>>> master
+for _ in range(1700):
     event_date = generate_data.gen_event_date()
     timestamp = generate_data.gen_timestamp()
     user_id = generate_data.gen_user_id()
@@ -34,19 +31,20 @@ for _ in range(1000):
 
     # Kiểm tra event_name và gửi vào topic phù hợp
     event = event_name["event_name"]
-    if event in {"view", "click"}:
+    print("---------------------")
+    print(event)
+    if event[:3] == "ev1":
         producer.send(TOPIC_1, value=data)
-        print(f"🔥 Sent to {TOPIC_1}: {json.dumps(data, indent=4)}")
-    elif event == "purchase":
+        print(f"🛒 Sent to topic_1: {json.dumps(data, indent=4)}")
+    elif event[:3] == "ev2":
         producer.send(TOPIC_2, value=data)
-        print(f"🛒 Sent to {TOPIC_2}: {json.dumps(data, indent=4)}")
+        print(f"🔥 Sent to topic_2: {json.dumps(data, indent=4)}")
+    elif event[:3] == "ev3":
+        producer.send(TOPIC_3, value=data)
+        print(f"🤖 Sent to topic_3: {json.dumps(data, indent=4)}")
 
     # Chờ 1 giây trước khi gửi tiếp
-<<<<<<< HEAD
     time.sleep(0.001)
-=======
-    time.sleep(0.00001)
->>>>>>> master
 
 # print("✅ Kafka Producer finished sending 10 messages.")
 
