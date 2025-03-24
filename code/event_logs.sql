f87ad871153b674deca164f39f3ff88cfde234b5
lehuynhduc <lhduc02@gmail.com>
2025-03-24 09:42:36 +0700

    update file .sql

--- code/event_logs.sql
+++ code/event_logs.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE IF NOT EXISTS event_logs;
 
 CREATE TABLE IF NOT EXISTS user_events (
-    event_date  DATE, 
-    timestamp   BIGINT, 
-    user_id     STRING, 
+    event_date  INT,
+    timestamp   BIGINT,
+    user_id     STRING,
     device      JSON,
     geo         JSON,
     event_name  STRING
